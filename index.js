047221f768df8f4c8894553bec9c2a0946b48371
WilliamBZA <william.brander@gmail.com>
2016-06-02 10:24:25 +0200

    Added facebook hook

--- index.js
+++ index.js
@@ -59,6 +59,14 @@ app.post('/post', function (req, res) {
   }
 });
 
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
