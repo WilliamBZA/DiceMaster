387c2389deb88cf0e5a00d0aa1d26bc8e7982fc2
WilliamBZA <william.brander@gmail.com>
2016-06-02 07:30:57 +0200

    Add attachment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 app.post('/post', function(req, res) {
       var body = {
         response_type: "in_channel",
-        text: "meow",
+        text: req.body.text,
         attachments: [{
           text: "You rolled a http://vignette4.wikia.nocookie.net/imperial-assault/images/d/d7/Imperial_Assault_Die_Face.png/revision/latest?cb=20150825022932"
         }]
